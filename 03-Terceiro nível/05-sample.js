3288a543de682918c8f11643087bd9c52c685dca
João Pedro <jpedrosdrummond@gmail.com>
2024-11-12 13:19:07 -0300

    Terceito nível modificado

--- 03-Terceiro nível/05-sample.js
+++ 03-Terceiro nível/05-sample.js
@@ -1,11 +1,14 @@
+const Url_Api_Veiculos = "https://swapi.dev/api/vehicles/";
+const Limite_Custo_Caros = 10000;
+
 async function buscarVeiculosECalcularCusto() {
     try {
-        const resposta = await fetch("https://swapi.dev/api/vehicles/");
+        const resposta = await fetch(Url_Api_Veiculos);
         const dados = await resposta.json();
 
-        const veiculosCaros = dados.results.filter(veiculo => parseInt(veiculo.cost_in_credits) > 10000);
+        const veiculosCaros = dados.results.filter(veiculo => parseInt(veiculo.cost_in_credits) > Limite_Custo_Caros);
 
-        console.log("Veículos caros (mais de 10.000 créditos):");
+        console.log(`Veículos caros (mais de ${Limite_Custo_Caros} créditos):`);
         veiculosCaros.forEach(veiculo => {
             console.log(`- ${veiculo.name}: ${veiculo.cost_in_credits} créditos`);
         });
@@ -22,3 +25,4 @@ async function buscarVeiculosECalcularCusto() {
 }
 
 buscarVeiculosECalcularCusto();
+
